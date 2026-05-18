67a0d433159c4632b74922cbe794a5b74a80a774
zhr136136 <1359510930@qq.com>
2026-05-18 10:24:41 +0800

    Add coroutine-lib-main dependency

---
+++ fiber_lib/build/CMakeFiles/coroutine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/haoran/coroutine-lib-main/fiber_lib/1thread/thread.cpp" "CMakeFiles/coroutine.dir/1thread/thread.cpp.o" "gcc" "CMakeFiles/coroutine.dir/1thread/thread.cpp.o.d"
+  "/home/haoran/coroutine-lib-main/fiber_lib/2fiber/fiber.cpp" "CMakeFiles/coroutine.dir/2fiber/fiber.cpp.o" "gcc" "CMakeFiles/coroutine.dir/2fiber/fiber.cpp.o.d"
+  "/home/haoran/coroutine-lib-main/fiber_lib/3scheduler/scheduler.cpp" "CMakeFiles/coroutine.dir/3scheduler/scheduler.cpp.o" "gcc" "CMakeFiles/coroutine.dir/3scheduler/scheduler.cpp.o.d"
+  "/home/haoran/coroutine-lib-main/fiber_lib/4timer/timer.cpp" "CMakeFiles/coroutine.dir/4timer/timer.cpp.o" "gcc" "CMakeFiles/coroutine.dir/4timer/timer.cpp.o.d"
+  "/home/haoran/coroutine-lib-main/fiber_lib/5iomanager/ioscheduler.cpp" "CMakeFiles/coroutine.dir/5iomanager/ioscheduler.cpp.o" "gcc" "CMakeFiles/coroutine.dir/5iomanager/ioscheduler.cpp.o.d"
+  "/home/haoran/coroutine-lib-main/fiber_lib/6hook/fd_manager.cpp" "CMakeFiles/coroutine.dir/6hook/fd_manager.cpp.o" "gcc" "CMakeFiles/coroutine.dir/6hook/fd_manager.cpp.o.d"
+  "/home/haoran/coroutine-lib-main/fiber_lib/6hook/hook.cpp" "CMakeFiles/coroutine.dir/6hook/hook.cpp.o" "gcc" "CMakeFiles/coroutine.dir/6hook/hook.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
